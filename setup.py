b576af85f81a992479454ca79bdcd585529e23db
Ying YinPo <inpool@126.com>
2017-10-09 09:25:52 +0800

    Change MANIFEST.in and description
    - Old MANIFEST.in didn't included the config.ini for unit test
    - Old description is too long to show on pypi webpage in one line

--- setup.py
+++ setup.py
@@ -8,33 +8,33 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 with open(os.path.join(HERE, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-
 REQUIRES = [
     'pyramid',
     'peewee',
 ]
 
-DESCRIPTION = ('A package which provides integration between the Pyramid web application '
-               'server and the peewee ORM.')
-setup(name='pyramid_peewee_conn',
-      version='0.7.0',
-      description=DESCRIPTION,
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-          "Topic :: Database"
-      ],
-      author='Inpool',
-      author_email='inpool@126.com',
-      url='https://github.com/inpool/pyramid_peewee_conn',
-      keywords='web pyramid peewee',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=REQUIRES,
-      extras_require={
-          'testing': ['pytest'],
-      })
+DESCRIPTION = (
+    'Provides integration between the Pyramid web application and the peewee ORM.'
+)
+setup(
+    name='pyramid_peewee_conn',
+    version='0.7.1',
+    description=DESCRIPTION,
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python", "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Database"
+    ],
+    author='Inpool',
+    author_email='inpool@126.com',
+    url='https://github.com/inpool/pyramid_peewee_conn',
+    keywords='web pyramid peewee',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIRES,
+    extras_require={
+        'testing': ['pytest'],
+    })
